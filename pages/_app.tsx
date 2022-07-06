5a0ee5ba2cc713f9f263508abc4aba3890a6466c
primaramadhaniputra <primaramadhanip@gmail.com>
2022-07-06 14:23:21 +0700

    add babel package

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
 import { GlobalStyle } from "atoms/GlobalStyles";
 import type { AppProps } from "next/app";
-import { useEffect } from "react";
 import { ToastContainer } from "react-toastify";
 import { RecoilRoot } from "recoil";
 import "../styles/globals.css";
@@ -8,30 +7,6 @@ import "../styles/react-toastify.css";
 
 function MyApp({ Component, pageProps, router }: AppProps) {
   console.log(router.pathname);
-  useEffect(() => {
-    Array.from(
-      document.querySelectorAll('head > link[rel="stylesheet"][data-n-p]')
-    ).forEach((node) => {
-      node.removeAttribute("data-n-p");
-    });
-    const mutationHandler = (mutations: any) => {
-      mutations.forEach(({ target }: any) => {
-        if (target.nodeName === "STYLE") {
-          if (target.getAttribute("media") === "x") {
-            target.removeAttribute("media");
-          }
-        }
-      });
-    };
-    const observer = new MutationObserver(mutationHandler);
-    observer.observe(document.head, {
-      subtree: true,
-      attributeFilter: ["media"],
-    });
-    return () => {
-      observer.disconnect();
-    };
-  }, []);
   return (
     <>
       <ToastContainer
