cf2622588a1a370212d9ca38950828b475c17b1d
primaramadhaniputra <primaramadhanip@gmail.com>
2022-07-06 15:11:32 +0700

    static position

--- src/components/organism/Navbar/styles.ts
+++ src/components/organism/Navbar/styles.ts
@@ -1,31 +1,31 @@
-import { Grid } from "@hudoro/neron";
-import styled from "styled-components";
-import { colors, mediaQueries } from "utils/styles";
+import { Grid } from '@hudoro/neron';
+import styled from 'styled-components';
+import { colors, mediaQueries } from 'utils/styles';
 
 export const NavbarWrapper = styled.div`
-  padding: 25px 32px;
-  position: absolute;
-  right: 0;
-  left: 0;
-  top: 0;
-  z-index: 999;
-  background-color: ${colors.primary};
-  ${mediaQueries.lg} {
-    position: static;
-    /* padding: 25px 100px; */
-    margin-top: 0;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-  }
+    padding: 25px 32px;
+    position: absolute;
+    right: 0;
+    left: 0;
+    top: 0;
+    z-index: 999;
+    background-color: ${colors.primary};
+    ${mediaQueries.lg} {
+        position: static !important;
+        padding: 25px 100px;
+        margin-top: 0;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+    }
 `;
 
 export const LinkWrapper = styled(Grid)`
-  margin-top: 40px;
-  display: flex;
-  flex-direction: column;
-  gap: 20px;
-  ${mediaQueries.lg} {
-    display: none;
-  }
+    margin-top: 40px;
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    ${mediaQueries.lg} {
+        display: none;
+    }
 `;
