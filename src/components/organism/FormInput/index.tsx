f69eec2023b3d9cdbae1cb6b26f7b4dfc1042f7a
primaramadhaniputra <primaramadhanip@gmail.com>
2022-07-08 11:20:09 +0700

    change ui

--- src/components/organism/FormInput/index.tsx
+++ src/components/organism/FormInput/index.tsx
@@ -7,7 +7,7 @@ import React, { FormEvent, useState } from "react";
 import { login } from "services/users";
 import { notify } from "utils/functions";
 import { fontSizing } from "utils/styles";
-import { RadioWrapper } from "./styles";
+import { RadioWrapper1, RadioWrapper2 } from "./styles";
 
 interface IProps {
   color?: string;
@@ -50,7 +50,7 @@ export default function FormInput({ color }: IProps) {
             name="password"
             type="password"
           />
-          <RadioWrapper
+          <RadioWrapper1
             container
             alignItems="center"
             justifyContent="space-between"
@@ -63,9 +63,23 @@ export default function FormInput({ color }: IProps) {
             <Text variant="p" style={{ fontSize: fontSizing.xs.fontSize }}>
               Forgot password ?
             </Text>
-          </RadioWrapper>
+          </RadioWrapper1>
+          <RadioWrapper2
+            container
+            alignItems="center"
+            justifyContent="space-between"
+          >
+            <Radio
+              label="Remember me"
+              onClick={handleRadio}
+              checked={checkedRadio}
+            />
+            <Text variant="p" style={{ fontSize: fontSizing.xs.fontSize }}>
+              Forgot password ?
+            </Text>
+          </RadioWrapper2>
         </Grid>
-        <StyledButton>Masuk</StyledButton>
+        <StyledButton>Login</StyledButton>
       </Grid>
     </form>
   );
