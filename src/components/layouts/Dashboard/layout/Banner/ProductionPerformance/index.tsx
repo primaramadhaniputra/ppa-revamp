eddec392c3dd48d68c6b8e617876aeb3709a9423
primaramadhaniputra <primaramadhanip@gmail.com>
2022-07-11 13:42:24 +0700

    add icons

--- src/components/layouts/Dashboard/layout/Banner/ProductionPerformance/index.tsx
+++ src/components/layouts/Dashboard/layout/Banner/ProductionPerformance/index.tsx
@@ -1,8 +1,10 @@
 import { Grid } from "@hudoro/neron";
 import TitlePage from "atoms/TitlePage";
+import Image from "next/image";
 import React from "react";
 import LabelValue from "src/components/organism/LabelValue";
 import { ProductionPerfromanceData } from "utils/dummy";
+import { colors } from "utils/styles";
 import { Item, ItemContainer } from "../styles";
 
 export default function ProductionPerformance() {
@@ -17,7 +19,16 @@ export default function ProductionPerformance() {
           return (
             <Item key={index}>
               <Grid container flexDirection="column" gap={20}>
-                <TitlePage type="h4">{data.title}</TitlePage>
+                <Grid
+                  container
+                  justifyContent="space-between"
+                  alignItems="center"
+                >
+                  <TitlePage styles={{ color: colors.orange }} type="h4">
+                    {data.title}
+                  </TitlePage>
+                  <Image src={`/icons/${data.icon}`} width={30} height={30} />
+                </Grid>
                 <Grid container flexDirection="column" gap={15}>
                   <LabelValue data={data.plan} />
                   <LabelValue data={data.act} />
