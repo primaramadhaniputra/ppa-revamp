3fe57f0e6a7e11cd122b2343b3f889f05268f191
primaramadhaniputra <primaramadhanip@gmail.com>
2022-07-26 11:42:23 +0700

    save chagnes

--- src/views/TeamWork/Report/AttendanceRatio/TopFilter/index.tsx
+++ src/views/TeamWork/Report/AttendanceRatio/TopFilter/index.tsx
@@ -5,7 +5,11 @@ import StyledSelect from 'molecules/StyledSelect'
 import React, { useState } from 'react'
 import { colors, fontWeights } from 'utils/styles'
 
-export default function TopFilter() {
+interface IProps {
+   noDept?: boolean
+}
+
+export default function TopFilter({ noDept }: IProps) {
    const [toDateState, setToDateState] = useState(new Date())
    const [fromDateState, setFromDateState] = useState(new Date())
    const handleToDateState = (e: Date) => {
@@ -18,7 +22,7 @@ export default function TopFilter() {
       <Grid container style={{ marginTop: '10px', gap: 5 }}>
          <DateCalendar title="To" dateState={toDateState} setDateState={handleToDateState} />
          <DateCalendar title="From" dateState={fromDateState} setDateState={handleFromDateState} />
-         <StyledSelect title='Dept' />
+         {!noDept && <StyledSelect title='Dept' />}
          <StyledSelect title='Comp' />
          <Grid container style={{ flex: 1, minWidth: '150px' }}>
             <StyledButton
