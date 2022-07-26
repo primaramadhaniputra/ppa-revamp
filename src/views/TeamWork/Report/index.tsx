176f878f1e17e2c365eca6a06f1cd61316d81514
primaramadhaniputra <primaramadhanip@gmail.com>
2022-07-26 16:34:37 +0700

    slicing

--- src/views/TeamWork/Report/index.tsx
+++ src/views/TeamWork/Report/index.tsx
@@ -1,27 +1,22 @@
 import { ISelectItem, Select } from "@hudoro/neron";
 import React, { useState } from "react";
 import Layout from "src/components/layouts/Dashboard/layout";
+import AttendanceDetail from "./AttendanceDetail";
 import AttendanceRatio from "./AttendanceRatio";
 import { ArrowDown, SelectContainer } from "./styles";
 
 
 const selectItems = [
   { id: 1, values: 'Attendance Ratio', label: 'Attendance Ratio' },
-  { id: 2, values: 'Update Roster', label: 'Update Roster' },
-  { id: 3, values: 'Set Roster', label: 'Set Roster' },
-  { id: 4, values: 'Attendance Revision', label: 'Attendance Revision' },
-  { id: 5, values: 'Upload SPL', label: 'Upload SPL' },
-  { id: 6, values: 'Upload DAR', label: 'Upload DAR' },
-  { id: 7, values: 'Leaving Application', label: 'Leaving Application' },
-  { id: 8, values: 'Benefit Claim Voucher', label: 'Benefit Claim Voucher' },
-  { id: 9, values: 'Surat Perintah Lembur', label: 'Surat Perintah Lembur' },
-  { id: 10, values: 'Surat Pelanggaran', label: 'Surat Pelanggaran' },
-  { id: 11, values: 'Employee Mutation', label: 'Employe Mutation' },
+  { id: 2, values: 'Attendance Detail', label: 'Attendance Detail' },
 ]
 
 const renderContent = (type: string) => {
   if (type === 'Attendance Ratio') {
     return <AttendanceRatio />
+  }
+  if (type === 'Attendance Detail') {
+    return <AttendanceDetail />
   }
 
 }
