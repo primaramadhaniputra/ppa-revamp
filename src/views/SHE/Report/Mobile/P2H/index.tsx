67614d8e7563ebc98ed48efb44de1a3365c7bfdb
primaramadhaniputra <primaramadhanip@gmail.com>
2022-08-01 09:24:04 +0700

    slicing

--- src/views/SHE/Report/Mobile/P2H/index.tsx
+++ src/views/SHE/Report/Mobile/P2H/index.tsx
@@ -1,8 +1,17 @@
 import React, { useState } from 'react'
 import Achievement from './Achievement'
 import { TabContainer, TabText, Wrapper } from './styles'
+import Temuan from './Temuan'
 
 const p2hTab = ['Detail', 'Monitoring', 'Temuan', 'Achievement']
+
+function renderTabContent(e: number) {
+   if (e === 3) {
+      return <Achievement />
+   } else if (e === 2) {
+      return <Temuan />
+   }
+}
 
 export default function P2H() {
    const [activeTab, setActiveTab] = useState(3)
@@ -10,6 +19,7 @@ export default function P2H() {
    const handleActiveTab = (index: number) => {
       setActiveTab(index)
    }
+
    return (
       <Wrapper>
          <TabContainer>
@@ -17,7 +27,7 @@ export default function P2H() {
                p2hTab.map((item, index) => <TabText key={index} activeTab={activeTab === index ? true : false} onClick={() => handleActiveTab(index)}  >{item}</TabText >)
             }
          </TabContainer>
-         {activeTab === 3 && <Achievement />}
+         {renderTabContent(activeTab)}
       </Wrapper>
    )
 }
