181887ae147f3eab6f7d4e64b93caeef88a13c70
primaramadhaniputra <primaramadhanip@gmail.com>
2022-08-03 14:47:36 +0700

    revisiiiiiii 19

--- src/views/TeamWork/Report/OperatorHours/styles.ts
+++ src/views/TeamWork/Report/OperatorHours/styles.ts
@@ -1,6 +1,6 @@
-import { Card, fontFamilies } from '@hudoro/neron';
+import { Card, fontFamilies, Grid, Text } from '@hudoro/neron';
 import styled from 'styled-components';
-import { fontSizing, mediaQueries } from 'utils/styles';
+import { fontSizing, fontWeights, mediaQueries } from 'utils/styles';
 
 export const SelectContainer = styled.div`
     position: relative;
@@ -35,12 +35,42 @@ export const StyledSpan = styled.span`
 `;
 
 export const Wrapper = styled(Card)`
-    margin-top: 30px;
+    margin-top: 10px;
     padding: 0;
     box-shadow: none;
     border-radius: 3px;
-    ${mediaQueries.sm} {
+    background-color: transparent;
+    /* ${mediaQueries.sm} {
         box-shadow: 0 0 25px rgba(0, 0, 0, 0.15);
         padding: 20px;
+    } */
+`;
+
+export const WrapperTitle = styled(Grid).attrs({
+    container: true,
+})`
+    gap: 20px;
+    justify-content: space-between;
+    align-items: center;
+    ${mediaQueries.md} {
+        background-color: white;
+        padding: 10px 15px;
+    }
+`;
+export const WrapperTable = styled(Grid)`
+    margin-top: 20px;
+    ${mediaQueries.md} {
+        background-color: white;
+        padding: 15px;
+    }
+`;
+
+export const TitleText = styled(Text).attrs({
+    variant: 'h3',
+})`
+    font-weight: ${fontWeights.bold};
+    font-size: 18px;
+    ${mediaQueries.md} {
+        font-size: 22px;
     }
 `;
