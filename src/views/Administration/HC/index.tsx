f7eec942ce00a5fe6d5999be2d9cd209813607a9
primaramadhaniputra <primaramadhanip@gmail.com>
2022-08-05 09:05:09 +0700

    refactor 3

--- src/views/Administration/HC/index.tsx
+++ src/views/Administration/HC/index.tsx
@@ -1,21 +1,11 @@
 import { Grid } from "@hudoro/neron";
 import React from "react";
 import Layout from "src/components/layouts/Dashboard/layout";
-import SearchingForm from "src/components/organism/SearchingForm";
-import { dummyInputDropdown } from "utils/dummy";
 
 export default function HC() {
   return (
     <Layout title="Administration / HC">
       <Grid style={{ maxWidth: "300px" }}>
-        <SearchingForm
-          title="Menu"
-          placeholder="Device / Production / Payload"
-          isDate={true}
-          isMenu={true}
-          isShift={false}
-          dropDownData={dummyInputDropdown}
-        />
       </Grid>
     </Layout>
   );
