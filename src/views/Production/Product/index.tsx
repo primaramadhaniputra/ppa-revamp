e1d390416e8c6b8d47319839cb8b2e8efbcb54ca
primaramadhaniputra <primaramadhanip@gmail.com>
2022-08-15 14:19:55 +0700

    slicing

--- src/views/Production/Product/index.tsx
+++ src/views/Production/Product/index.tsx
@@ -1,13 +1,11 @@
-import { Grid } from '@hudoro/neron'
+import { Grid, Text } from '@hudoro/neron'
 import Image from 'next/image'
 import React from 'react'
-import { HeaderContainer, ProductText, ProductTextContainer, SingleProduct, Wrapper } from './styles'
+import { useWindowSize } from 'utils/functions'
+import { fontWeights } from 'utils/styles'
+import { HeaderContainer, ProductText, ProductTextContainer, SingleProduct, SpanHeader, Wrapper } from './styles'
 
 const data = [
-   {
-      title: 'All Site',
-      logo: '/images/production1.png'
-   },
    {
       title: 'BIB',
       logo: '/images/production3.png'
@@ -51,47 +49,87 @@ const data = [
 ]
 
 export default function Product() {
+   const { width } = useWindowSize()
    return (
-      <Wrapper>
-         {
-            data.map((item, index) =>
-               <SingleProduct key={index}>
-                  <HeaderContainer>
-                     <ProductText>
-                        OB kbcm <span style={{ color: '#47BF34' }}>(105%)</span>
-                     </ProductText>
-                     <Image src={item.logo} height={37} width={37} alt='product logo' quality={100} />
-                     <ProductText>
-                        Coal kton <span style={{ color: '#EB3B3B' }}>(105%)</span>
-                     </ProductText>
-                  </HeaderContainer>
-                  <ProductTextContainer>
-                     <Grid container style={{ flex: 1 }} flexDirection='column' gap={10} >
-                        <Grid container justifyContent='space-between' style={{ flex: 1, maxWidth: '150px' }}>
-                           <ProductText>P</ProductText>
-                           <ProductText>908</ProductText>
+      <>
+         <SingleProduct style={{ maxWidth: '500px', marginTop: '30px' }} >
+            <HeaderContainer>
+               <ProductText variant='header' style={{ fontWeight: fontWeights.medium }}>
+                  OB kbcm <SpanHeader style={{ backgroundColor: '#47BF34' }}>(105%)</SpanHeader>
+               </ProductText>
+               <Image src='/images/production1.png' height={width > 350 ? 37 : 20} width={width > 350 ? 37 : 22} alt='product logo' quality={100} />
+               <ProductText variant='header' style={{ fontWeight: fontWeights.medium }}>
+                  Coal kton <SpanHeader style={{ backgroundColor: '#EB3B3B' }}>(105%)</SpanHeader>
+               </ProductText>
+            </HeaderContainer>
+            <ProductTextContainer>
+               <Grid container style={{ flex: 1 }} flexDirection='column' gap={10} >
+                  <Grid container justifyContent='space-between' style={{ flex: 1, maxWidth: '150px' }}>
+                     <Image src='/images/Ic-Target.png' height={20.5} width={16} alt='product logo' quality={100} />
+                     <ProductText>908</ProductText>
+                  </Grid>
+                  <Grid container justifyContent='space-between' style={{ flex: 1, maxWidth: '150px' }}>
+                     <Image src='/images/Ic-Trophy.png' height={20.5} width={16} alt='product logo' quality={100} />
+                     <ProductText>908</ProductText>
+                  </Grid>
+               </Grid>
+               <Grid container style={{ flex: 1 }} justifyContent='center' alignItems='center'>
+                  <ProductText>adf</ProductText>
+               </Grid>
+               <Grid container style={{ flex: 1 }} flexDirection='column' gap={10} >
+                  <Grid container justifyContent='space-between' style={{ flex: 1, maxWidth: '150px' }}>
+                     <Image src='/images/Ic-Target.png' height={20.5} width={16} alt='product logo' quality={100} />
+                     <ProductText>408</ProductText>
+                  </Grid>
+                  <Grid container justifyContent='space-between' style={{ flex: 1, maxWidth: '150px' }}>
+                     <Image src='/images/Ic-Trophy.png' height={20.5} width={16} alt='product logo' quality={100} />
+                     <ProductText>400</ProductText>
+                  </Grid>
+               </Grid>
+            </ProductTextContainer>
+         </SingleProduct>
+         <Text variant='h3' style={{ fontWeight: fontWeights.bold, marginTop: '50px' }} >Sites</Text>
+         <Wrapper>
+            {
+               data.map((item, index) =>
+                  <SingleProduct key={index} >
+                     <HeaderContainer>
+                        <ProductText variant='header' style={{ fontWeight: fontWeights.medium }}>
+                           OB kbcm <SpanHeader style={{ backgroundColor: '#47BF34' }}>(105%)</SpanHeader>
+                        </ProductText>
+                        <Image src={item.logo} height={width > 350 ? 37 : 20} width={width > 350 ? 37 : 22} alt='product logo' quality={100} />
+                        <ProductText variant='header' style={{ fontWeight: fontWeights.medium }}>
+                           Coal kton <SpanHeader style={{ backgroundColor: '#EB3B3B' }}>(105%)</SpanHeader>
+                        </ProductText>
+                     </HeaderContainer>
+                     <ProductTextContainer>
+                        <Grid container style={{ flex: 1 }} flexDirection='column' gap={10} >
+                           <Grid container justifyContent='space-between' style={{ flex: 1, maxWidth: '150px' }}>
+                              <Image src='/images/Ic-Target.png' height={20.5} width={16} alt='product logo' quality={100} />
+                              <ProductText>908</ProductText>
+                           </Grid>
+                           <Grid container justifyContent='space-between' style={{ flex: 1, maxWidth: '150px' }}>
+                              <Image src='/images/Ic-Trophy.png' height={20.5} width={16} alt='product logo' quality={100} />
+                              <ProductText>908</ProductText>
+                           </Grid>
                         </Grid>
-                        <Grid container justifyContent='space-between' style={{ flex: 1, maxWidth: '150px' }}>
-                           <ProductText>A</ProductText>
-                           <ProductText>908</ProductText>
+                        <Grid container style={{ flex: 1 }} justifyContent='center' alignItems='center'>
+                           <ProductText>{item.title}</ProductText>
                         </Grid>
-                     </Grid>
-                     <Grid container style={{ flex: 1 }} justifyContent='center' alignItems='center'>
-                        <ProductText>{item.title}</ProductText>
-                     </Grid>
-                     <Grid container style={{ flex: 1 }} flexDirection='column' gap={10} >
-                        <Grid container justifyContent='space-between' style={{ flex: 1, maxWidth: '150px' }}>
-                           <ProductText>P</ProductText>
-                           <ProductText>408</ProductText>
+                        <Grid container style={{ flex: 1 }} flexDirection='column' gap={10} >
+                           <Grid container justifyContent='space-between' style={{ flex: 1, maxWidth: '150px' }}>
+                              <Image src='/images/Ic-Target.png' height={20.5} width={16} alt='product logo' quality={100} />
+                              <ProductText>408</ProductText>
+                           </Grid>
+                           <Grid container justifyContent='space-between' style={{ flex: 1, maxWidth: '150px' }}>
+                              <Image src='/images/Ic-Trophy.png' height={20.5} width={16} alt='product logo' quality={100} />
+                              <ProductText>400</ProductText>
+                           </Grid>
                         </Grid>
-                        <Grid container justifyContent='space-between' style={{ flex: 1, maxWidth: '150px' }}>
-                           <ProductText>A</ProductText>
-                           <ProductText>400</ProductText>
-                        </Grid>
-                     </Grid>
-                  </ProductTextContainer>
-               </SingleProduct>)
-         }
-      </Wrapper>
+                     </ProductTextContainer>
+                  </SingleProduct>)
+            }
+         </Wrapper>
+      </>
    )
 }
--- src/views/Production/index.tsx
+++ src/views/Production/index.tsx
@@ -1,27 +1,27 @@
-// import { Grid, Text, Toggler } from "@hudoro/neron";
-// import DateCalendar from "molecules/Date";
-import React from "react";
+import { Grid, Text, Toggler } from "@hudoro/neron";
+import DateCalendar from "molecules/Date";
+import React, { useState } from "react";
 import Layout from "src/components/layouts/Dashboard/layout";
-// import Product from "./Product";
+import Product from "./Product";
 import { WrapperDate } from "./styles";
 
 export default function Production() {
-   // const [date, setDate] = useState(new Date())
+   const [date, setDate] = useState(new Date())
 
 
    return (
       <Layout title="Production / Report">
          <WrapperDate>
-            {/* <Grid container gap={5} alignItems="flex-end" style={{ height: '47px', justifyContent: 'end' }} >
+            <Grid container gap={5} alignItems="flex-end" style={{ height: '47px', justifyContent: 'end' }} >
                <Text variant="h4" style={{ fontSize: '14px' }}>TC</Text>
                <Toggler />
                <Text variant="h4" style={{ fontSize: '14px' }}>JS</Text>
             </Grid>
             <Grid container >
                <DateCalendar dateState={date} setDateState={setDate} title="Date" />
-            </Grid> */}
+            </Grid>
          </WrapperDate>
-         {/* <Product /> */}
+         <Product />
       </Layout>
    );
 }
