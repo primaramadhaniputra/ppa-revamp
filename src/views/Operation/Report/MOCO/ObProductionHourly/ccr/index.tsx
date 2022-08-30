81465d1373d4dab9446314cf8073a8700839cec6
primaramadhaniputra <primaramadhanip@gmail.com>
2022-08-30 09:34:39 +0700

    fix

--- src/views/Operation/Report/MOCO/ObProductionHourly/ccr/index.tsx
+++ src/views/Operation/Report/MOCO/ObProductionHourly/ccr/index.tsx
@@ -101,12 +101,8 @@ export default function CCR() {
   }
 
   const exportFile = () => {
-    let lyrics =
-      "But still I'm having memories of high speeds when the cops crashed\n" +
-      "As I laugh, pushin the gas while my Glocks blast\n" +
-      "We was young and we was dumb but we had heart";
-
-    var blob = new Blob([lyrics], { type: "text/plain;charset=utf-8" });
+    const data = defaultDataTable.map(data => data[10])
+    var blob = new Blob(data, { type: "text/plain;charset=utf-8" });
     saveAs(blob, "testfile1.txt");
   }
 
