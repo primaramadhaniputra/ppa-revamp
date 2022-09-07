f649bcf62ea475c153cf7c16917c00d8615cd32e
primaramadhaniputra <primaramadhanip@gmail.com>
2022-09-07 09:19:09 +0700

    zoom in,zoom out,reset zoom

--- src/components/atoms/Icon/index.ts
+++ src/components/atoms/Icon/index.ts
@@ -3,7 +3,21 @@ import IcCalendar from "./IcCalendar";
 import IcClose from "./IcClose";
 import IcEdit from "./IcEdit";
 import IcImage from "./IcImage";
+import IcMinusCircle from "./IcMinusCircle";
+import IcPlusCircle from "./IcPlusCircle";
 import IcPrinter from "./IcPrinter";
 import IcRefresh from "./IcRefresh";
+import IcReset from "./IcReset";
 
-export { IcClose, IcEdit, IcRefresh, IcBan, IcCalendar, IcImage, IcPrinter };
+export {
+  IcClose,
+  IcEdit,
+  IcRefresh,
+  IcBan,
+  IcCalendar,
+  IcImage,
+  IcPrinter,
+  IcPlusCircle,
+  IcMinusCircle,
+  IcReset,
+};
