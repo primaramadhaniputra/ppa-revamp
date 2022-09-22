3f313d36cdcbd75e28ec593fd2a70ac977c9c647
primaramadhaniputra <primaramadhanip@gmail.com>
2022-09-22 08:26:15 +0700

    adding temporary logo and chang logo name

--- src/views/Production/Product/TotalSites/index.tsx
+++ src/views/Production/Product/TotalSites/index.tsx
@@ -9,12 +9,12 @@ interface IProps {
 	totalDataSites: ITotalAllSites;
 }
 
-const iconUrl = ['CheckCircle.png', 'coal.png', 'coal.png']
-const gridArea = ['ob', 'coal', 'ore']
+const iconUrl = ["CheckCircle.png", "coal.png", "coal.png"];
+const gridArea = ["ob", "coal", "ore"];
 
 export default function TotalSites({ totalDataSites }: IProps) {
-	const obj = totalDataSites ? Object.entries(totalDataSites) : []
-	console.log(obj.map(item => console.log(item[1])))
+	const obj = totalDataSites ? Object.entries(totalDataSites) : [];
+
 	return (
 		<Wrapper>
 			<Grid style={{ marginBottom: "31px" }} container alignItems="center" gap={15}>
@@ -22,45 +22,41 @@ export default function TotalSites({ totalDataSites }: IProps) {
 				<Image src="/logo/production2.png" width={37} height={37} alt="ppa logo" />
 			</Grid>
 			<WrapperProduct>
-				{
-					obj.length > 0 && obj.map((item, index) =>
-						<SingleProduct style={{ gridArea: gridArea[index] }}>
-							<Grid container alignItems="center" gap={10}>
-								<Image src={`/icons/${iconUrl[index]}`} width={28} height={28} alt="circle logo" />
-								<Title>{item[0]} Kbcm</Title>
+				{obj.map((item, index) => (
+					<SingleProduct style={{ gridArea: gridArea[index] }}>
+						<Grid container alignItems="center" gap={10}>
+							<Image src={`/icons/${iconUrl[index]}`} width={28} height={28} alt="circle logo" />
+							<Title>{item[0]} Kbcm</Title>
+						</Grid>
+						<Lozenge>{item[1].percent}%</Lozenge>
+						<Grid container justifyContent="space-between" style={{ marginBottom: "20px" }}>
+							<Grid container gap={5}>
+								<Image
+									src="/icons/Ic-Target.png"
+									height={16}
+									width={16}
+									alt="product logo"
+									quality={100}
+								/>
+								<ProductText>P</ProductText>
 							</Grid>
-							<Lozenge>{item[1].percent}%</Lozenge>
-							<Grid container justifyContent="space-between" style={{ marginBottom: "20px" }}>
-								<Grid container gap={5}>
-									<Image
-										src="/icons/Ic-Target.png"
-										height={16}
-										width={16}
-										alt="product logo"
-										quality={100}
-									/>
-									<ProductText>P</ProductText>
-								</Grid>
-								<ProductText>{item[1].plan}</ProductText>
+							<ProductText>{item[1].plan}</ProductText>
+						</Grid>
+						<Grid container justifyContent="space-between">
+							<Grid container gap={5}>
+								<Image
+									src="/icons/Ic-Trophy.png"
+									height={20.5}
+									width={16}
+									alt="product logo"
+									quality={100}
+								/>
+								<ProductText>A</ProductText>
 							</Grid>
-							<Grid container justifyContent="space-between">
-								<Grid container gap={5}>
-									<Image
-										src="/icons/Ic-Trophy.png"
-										height={20.5}
-										width={16}
-										alt="product logo"
-										quality={100}
-									/>
-									<ProductText>A</ProductText>
-								</Grid>
-								<ProductText>
-									{item[1].production}
-								</ProductText>
-							</Grid>
-						</SingleProduct>
-					)
-				}
+							<ProductText>{item[1].production}</ProductText>
+						</Grid>
+					</SingleProduct>
+				))}
 			</WrapperProduct>
 		</Wrapper>
 	);
