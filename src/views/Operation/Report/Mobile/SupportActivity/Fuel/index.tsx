1b3a3b37b74094c239bdb51950c329575f6b5311
primaramadhaniputra <primaramadhanip@gmail.com>
2022-10-03 09:59:29 +0700

    add new tab component,add view for page operation/report/mobile/support activity/fuel/refuelling

--- src/views/Operation/Report/Mobile/SupportActivity/Fuel/index.tsx
+++ src/views/Operation/Report/Mobile/SupportActivity/Fuel/index.tsx
@@ -1,23 +1,26 @@
 import TabV4 from "molecules/TabV4";
 import React, { useState } from "react";
+import Refueling from "./Refueling";
 import { Wrapper } from "./styles";
 import Summary from "./Summary";
 
 const tabText = ["SUMMARY", "REFUELING", "FLOWMETER"];
 
 const renderContent = (type: string) => {
-	if (type === "SUMMARY") {
-		return <Summary />;
-	}
+   if (type === "SUMMARY") {
+      return <Summary />;
+   } else if (type === "REFUELING") {
+      return <Refueling />;
+   }
 };
 
 export default function Fuel() {
-	const [activeTab, setActiveTab] = useState(0);
+   const [activeTab, setActiveTab] = useState(0);
 
-	return (
-		<Wrapper>
-			<TabV4 tabText={tabText} activeTab={activeTab} setActiveTab={setActiveTab} />
-			{renderContent(tabText[activeTab])}
-		</Wrapper>
-	);
+   return (
+      <Wrapper>
+         <TabV4 tabText={tabText} activeTab={activeTab} setActiveTab={setActiveTab} />
+         {renderContent(tabText[activeTab])}
+      </Wrapper>
+   );
 }
