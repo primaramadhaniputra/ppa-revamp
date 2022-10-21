982940fb63821aee3d89b8c9a9b1d12b78262bc9
primaramadhaniputra <primaramadhanip@gmail.com>
2022-10-21 13:59:58 +0700

    remove unnecassry comp,add simple animation hover to button

--- src/components/atoms/StyledButton/styles.ts
+++ src/components/atoms/StyledButton/styles.ts
@@ -14,10 +14,9 @@ export const StyledButton = styled.button`
 	font-size: ${fontSizing.sm.fontSize};
 	text-align: center;
 	color: white;
+	transition: 0.3s;
 	&:hover {
-		opacity: 0.8;
-	}
-	&:active {
-		opacity: 1;
+		transform: translateY(-3px);
+		box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.5);
 	}
 `;
