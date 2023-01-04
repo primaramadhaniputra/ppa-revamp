156c9876f6add602008f9b4226dfd7e4aeee1d8d
primaramadhaniputra <77718493+primaramadhaniputra@users.noreply.github.com>
2023-01-04 15:28:54 +0700

    Fitur feedback (#229)
    * migrate table page teamwork / fomr
    
    * migrate table page teamwork / report 1
    
    * migrate table page teamwork / report 2
    
    * migrate table page system
    
    * migrate table page she/form
    
    * migrate table page she/report
    
    * migrate table page operation/report 1
    
    * migrate table page operation / report
    
    * migrate table page operation / data entry 1
    
    * migrate table page operation / data entry 2
    
    * migrate table page operation / data entry 3
    
    * migrate table page administration
    
    * remove old table
    
    * fix err

--- src/views/Administration/Legal/FlyingForm/index.tsx
+++ src/views/Administration/Legal/FlyingForm/index.tsx
@@ -11,55 +11,54 @@ interface IProps {
 
 const dummyTextPersetujuan = [
 	{
-		title: 'MASA BERLAKU',
-		desc: '2022-08-17 - 2022-10-24'
+		title: "MASA BERLAKU",
+		desc: "2022-08-17 - 2022-10-24",
 	},
 	{
-		title: 'CATATAN PERSETUJUAN',
-		desc: 'Banana'
+		title: "CATATAN PERSETUJUAN",
+		desc: "Banana",
 	},
-]
+];
 const dummyText = [
 	{
-		title: 'NO. DOKUMEN',
-		desc: 'Doc123'
+		title: "NO. DOKUMEN",
+		desc: "Doc123",
 	},
 	{
-		title: 'NAMA DOKUMEN',
-		desc: 'perjanjian'
+		title: "NAMA DOKUMEN",
+		desc: "perjanjian",
 	},
 	{
-		title: 'DITERIMA DARI/PELANGGAN',
-		desc: 'abn'
+		title: "DITERIMA DARI/PELANGGAN",
+		desc: "abn",
 	},
 	{
-		title: 'SUMBER DOKUMEN',
-		desc: 'Pelanggan'
+		title: "SUMBER DOKUMEN",
+		desc: "Pelanggan",
 	},
 	{
-		title: 'PERMOHONAN',
-		desc: ''
+		title: "PERMOHONAN",
+		desc: "",
 	},
 	{
-		title: 'URAIAN',
-		desc: 'test lagi'
+		title: "URAIAN",
+		desc: "test lagi",
 	},
 	{
-		title: 'URAIAN',
-		desc: 'test lagi'
+		title: "URAIAN",
+		desc: "test lagi",
 	},
 	{
-		title: 'PERUNTUKAN',
-		desc: 'Internal'
+		title: "PERUNTUKAN",
+		desc: "Internal",
 	},
 	{
-		title: 'CATATAN PERMOHONAN',
-		desc: 'test input'
+		title: "CATATAN PERMOHONAN",
+		desc: "test input",
 	},
-]
+];
 
 export default function FlyingForm({ isShowDetail, setIsShowDetail, formPosition }: IProps) {
-
 	return (
 		<>
 			<LayoutOverlayData
@@ -69,11 +68,20 @@ export default function FlyingForm({ isShowDetail, setIsShowDetail, formPosition
 				title="PERMOHONAN AKSES"
 			>
 				<Grid style={{ padding: "0 10px" }}>
-					<TextBetween dummyData={dummyText} maxLabelWidth='210px' />
+					<TextBetween dummyData={dummyText} maxLabelWidth="210px" />
 				</Grid>
-				<Grid style={{ padding: " 20px 10px", marginTop: '40px', borderTop: '1px solid black', borderBottom: '1px solid black' }}>
-					<Text variant="h4" style={{ textAlign: 'center', marginBottom: '30px' }}>Persetujuan</Text>
-					<TextBetween dummyData={dummyTextPersetujuan} maxLabelWidth='210px' />
+				<Grid
+					style={{
+						padding: " 20px 10px",
+						marginTop: "40px",
+						borderTop: "1px solid black",
+						borderBottom: "1px solid black",
+					}}
+				>
+					<Text variant="h4" style={{ textAlign: "center", marginBottom: "30px" }}>
+						Persetujuan
+					</Text>
+					<TextBetween dummyData={dummyTextPersetujuan} maxLabelWidth="210px" />
 				</Grid>
 			</LayoutOverlayData>
 		</>
