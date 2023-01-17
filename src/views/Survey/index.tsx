7911a130d05570f740ce2fa0cf40b46b519669aa
primaramadhaniputra <primaramadhanip@gmail.com>
2023-01-17 13:57:29 +0700

    add survey page 1

---
+++ src/views/Survey/index.tsx
@@ -0,0 +1,12 @@
+import TitlePage from "atoms/TitlePage";
+import React from "react";
+
+const Survey = () => {
+	return (
+		<TitlePage type="h3" styles={{ fontSize: "22px" }}>
+			Survey Kepuasan Pelanggan
+		</TitlePage>
+	);
+};
+
+export default Survey;
