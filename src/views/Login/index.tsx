8a03c34fa5787b2aaca50d4d4ac80ef5316ffe5c
primaramadhaniputra <primaramadhanip@gmail.com>
2023-01-17 08:33:19 +0700

    fix fcp login page 1

--- src/views/Login/index.tsx
+++ src/views/Login/index.tsx
@@ -24,30 +24,30 @@ interface IProps {
 	id: number;
 }
 
-export default function Home({ id }: IProps) {
-	const renderLayout = () => {
-		if (id === 1) {
-			return <Login1 />;
-		} else if (id === 2) {
-			return <Login2 />;
-		} else if (id === 3) {
-			return <Login22 />;
-		} else if (id === 4) {
-			return <Login3 />;
-		} else if (id === 5) {
-			return <Login31 />;
-		} else if (id === 6) {
-			return <Login4 />;
-		} else if (id === 7) {
-			return <Login41 />;
-		} else if (id === 8) {
-			return <Login5 />;
-		} else if (id === 9) {
-			return <Login51 />;
-		} else {
-			return <Login6 />;
-		}
-	};
+const RenderLayout = ({ id }: IProps) => {
+	if (id === 1) {
+		return <Login1 />;
+	} else if (id === 2) {
+		return <Login2 />;
+	} else if (id === 3) {
+		return <Login22 />;
+	} else if (id === 4) {
+		return <Login3 />;
+	} else if (id === 5) {
+		return <Login31 />;
+	} else if (id === 6) {
+		return <Login4 />;
+	} else if (id === 7) {
+		return <Login41 />;
+	} else if (id === 8) {
+		return <Login5 />;
+	} else if (id === 9) {
+		return <Login51 />;
+	} else {
+		return <Login6 />;
+	}
+};
 
-	return <>{renderLayout()}</>;
+export default function Home({ id }: IProps) {
+	return <RenderLayout id={id} />;
 }
