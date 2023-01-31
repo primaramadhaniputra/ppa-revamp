a5db48f82e0e058edeb178e4c8acb8ee224f78e1
primaramadhaniputra <primaramadhanip@gmail.com>
2023-01-31 09:14:34 +0700

    fix styling add new pacakge export excel

--- src/views/Survey/index.tsx
+++ src/views/Survey/index.tsx
@@ -20,6 +20,7 @@ const Survey = ({ periode }: IProps) => {
 			const response = await getReportCriteriaByCriteriaId({
 				path: `${periodeId}`,
 			});
+			console.log(response.data.data);
 			setReportCriteria(response.data.data.reports);
 		} catch (error) {
 			console.log("error", error);
