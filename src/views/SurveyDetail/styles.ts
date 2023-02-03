afc47b5e18fa2f86e6b0ab2411e977fd92e23451
primaramadhaniputra <primaramadhanip@gmail.com>
2023-02-03 07:25:35 +0700

    fix text detail description page survey detail

---
+++ src/views/SurveyDetail/styles.ts
@@ -0,0 +1,10 @@
+import { Text } from "@hudoro/neron";
+import styled from "styled-components";
+
+export const PeriodeText = styled(Text).attrs({
+	variant: "h4",
+})`
+	font-weight: 400;
+	font-size: 18px;
+	line-height: 36px;
+`;
