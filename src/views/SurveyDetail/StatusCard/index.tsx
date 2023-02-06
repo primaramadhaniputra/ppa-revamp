68e5a842cadcdcaa6b622fb2c0a15ad8bc91b452
primaramadhaniputra <primaramadhanip@gmail.com>
2023-02-06 15:20:17 +0700

    refactoring

--- src/views/SurveyDetail/StatusCard/index.tsx
+++ src/views/SurveyDetail/StatusCard/index.tsx
@@ -2,7 +2,7 @@ import { Grid } from "@hudoro/neron";
 import { IcAverage, IcScore, IcSum } from "atoms/Icon";
 import Cookies from "js-cookie";
 import React from "react";
-import { Container, IconContainer, StatusText, StyledCard, ValueText } from "./styles";
+import { Container, IconContainer, StatusText, StyledCard, ValueText, Wrapper } from "./styles";
 
 const renderTextAverage = (evaluationNumber: number) => {
 	if (evaluationNumber < 3) {
@@ -24,42 +24,40 @@ const StatusCard = () => {
 
 	const parseAverage = average ? Number(average) : 0;
 
+	const dataStatus = [
+		{
+			title: "Total",
+			value: total,
+			icon: <IcSum width={32} color="#2F88FF" />,
+		},
+		{
+			title: "Rata-Rata",
+			value: average,
+			icon: <IcAverage width={32} color="#2F88FF" />,
+		},
+		{
+			title: "Score",
+			value: parseAverage,
+			icon: <IcScore width={32} color="#2F88FF" />,
+		},
+	];
+
 	return (
-		<Container>
-			<StyledCard>
-				<Grid container gap={24} alignItems="center">
-					<IconContainer>
-						<IcSum width={32} color="#2F88FF" />
-					</IconContainer>
-					<Grid container flexDirection="column" gap={10}>
-						<StatusText>Total</StatusText>
-						<ValueText>{total || 0}</ValueText>
-					</Grid>
-				</Grid>
-			</StyledCard>
-			<StyledCard>
-				<Grid container gap={24} alignItems="center">
-					<IconContainer>
-						<IcAverage width={32} color="#2F88FF" />
-					</IconContainer>
-					<Grid container flexDirection="column" gap={10}>
-						<StatusText>Rata-Rata</StatusText>
-						<ValueText>{average || 0}</ValueText>
-					</Grid>
-				</Grid>
-			</StyledCard>
-			<StyledCard>
-				<Grid container gap={24} alignItems="center">
-					<IconContainer>
-						<IcScore width={32} color="#2F88FF" />
-					</IconContainer>
-					<Grid container flexDirection="column" gap={10}>
-						<StatusText>Score</StatusText>
-						<ValueText>{renderTextAverage(parseAverage)}</ValueText>
-					</Grid>
-				</Grid>
-			</StyledCard>
-		</Container>
+		<Wrapper>
+			{dataStatus.map((item, index) => (
+				<StyledCard key={index}>
+					<Container>
+						<IconContainer>{item.icon}</IconContainer>
+						<Grid container flexDirection="column" gap={10}>
+							<StatusText>{item.title}</StatusText>
+							<ValueText>
+								{item.title === "Score" ? renderTextAverage(item.value as number) : item.value}
+							</ValueText>
+						</Grid>
+					</Container>
+				</StyledCard>
+			))}
+		</Wrapper>
 	);
 };
 
