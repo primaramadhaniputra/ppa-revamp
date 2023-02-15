e5fd0570bf67a58426af28fb21d3e2d606708f07
primaramadhaniputra <primaramadhanip@gmail.com>
2023-02-15 16:22:39 +0700

    slicing partisiapn 2

--- src/views/SurveyDetail/CardDetail/Paritisipan/UserSurvey/HasilTest/index.tsx
+++ src/views/SurveyDetail/CardDetail/Paritisipan/UserSurvey/HasilTest/index.tsx
@@ -1,9 +1,35 @@
-import { Grid } from "@hudoro/neron";
+import { Grid, Icon } from "@hudoro/neron";
 import { IcScrollCheck } from "atoms/Icon";
-import React from "react";
-import { Container, Content, IconContainer, Title } from "./styles";
+import React, { useRef, useState } from "react";
+import {
+	Container,
+	Content,
+	IconContainer,
+	QuestionContainer,
+	QuestionType,
+	SingleQuestions,
+	SingleQuestionsContainer,
+	StyledText,
+	Title,
+} from "./styles";
+
+const ar = new Array(3).fill(0);
 
 const HasilTest = () => {
+	const [activeQuestionTab, setActiveQuestionTab] = useState(-1);
+	const [activeQuestionTabHeight, setActiveQuestionTabHeight] = useState(41);
+	const containerRef = useRef<HTMLDivElement>(null);
+
+	const handleChangeActiveTab = (idx: number) => {
+		const height = containerRef.current?.getBoundingClientRect().height || 0;
+		if (activeQuestionTab === idx) {
+			setActiveQuestionTab(-1);
+			return setActiveQuestionTabHeight(41);
+		}
+		setActiveQuestionTabHeight((value) => value + height);
+		return setActiveQuestionTab(idx);
+	};
+
 	return (
 		<Container>
 			<IconContainer>
@@ -11,7 +37,75 @@ const HasilTest = () => {
 			</IconContainer>
 			<Grid>
 				<Title>Hasil Test</Title>
-				<Content>she</Content>
+				<Content>
+					{ar.map((_, index) => (
+						<QuestionContainer
+							key={index}
+							activeQuestionTab={activeQuestionTab === index}
+							style={{ height: activeQuestionTab === index ? activeQuestionTabHeight : "41px" }}
+						>
+							<Grid container alignItems="center" justifyContent="space-between">
+								<QuestionType>SHE</QuestionType>
+								<Icon
+									iconName={activeQuestionTab === index ? "IcArrowUp" : "IcArrowDown"}
+									size={16}
+									style={{ cursor: "pointer" }}
+									onClick={() => handleChangeActiveTab(index)}
+								/>
+							</Grid>
+							<SingleQuestionsContainer ref={containerRef}>
+								<SingleQuestions>
+									<StyledText>Safety</StyledText>
+									<StyledText>2</StyledText>
+								</SingleQuestions>
+								<SingleQuestions>
+									<StyledText>Safety</StyledText>
+									<StyledText>2</StyledText>
+								</SingleQuestions>
+								<SingleQuestions>
+									<StyledText>Safety</StyledText>
+									<StyledText>2</StyledText>
+								</SingleQuestions>
+								<SingleQuestions>
+									<StyledText>Safety</StyledText>
+									<StyledText>2</StyledText>
+								</SingleQuestions>
+								<SingleQuestions>
+									<StyledText>Safety</StyledText>
+									<StyledText>2</StyledText>
+								</SingleQuestions>
+								<SingleQuestions>
+									<StyledText>Safety</StyledText>
+									<StyledText>2</StyledText>
+								</SingleQuestions>
+								<SingleQuestions>
+									<StyledText>Safety</StyledText>
+									<StyledText>2</StyledText>
+								</SingleQuestions>
+								<SingleQuestions>
+									<StyledText>Safety</StyledText>
+									<StyledText>2</StyledText>
+								</SingleQuestions>
+								<SingleQuestions>
+									<StyledText>Safety</StyledText>
+									<StyledText>2</StyledText>
+								</SingleQuestions>
+								<SingleQuestions>
+									<StyledText>Safety</StyledText>
+									<StyledText>2</StyledText>
+								</SingleQuestions>
+								<SingleQuestions>
+									<StyledText>Safety</StyledText>
+									<StyledText>2</StyledText>
+								</SingleQuestions>
+								<SingleQuestions>
+									<StyledText>Safety</StyledText>
+									<StyledText>2</StyledText>
+								</SingleQuestions>
+							</SingleQuestionsContainer>
+						</QuestionContainer>
+					))}
+				</Content>
 			</Grid>
 		</Container>
 	);
--- src/views/SurveyDetail/CardDetail/Paritisipan/UserSurvey/HasilTest/styles.ts
+++ src/views/SurveyDetail/CardDetail/Paritisipan/UserSurvey/HasilTest/styles.ts
@@ -1,5 +1,9 @@
-import { Text } from "@hudoro/neron";
+import { Grid, Text } from "@hudoro/neron";
 import styled from "styled-components";
+
+interface IProps {
+	activeQuestionTab?: boolean;
+}
 
 export const Container = styled.div`
 	border: 1px solid black;
@@ -33,4 +37,59 @@ export const Title = styled(Text).attrs({
 
 export const Content = styled.article`
 	margin-top: 34px;
+	display: flex;
+	flex-direction: column;
+	gap: 24px;
+`;
+
+export const QuestionContainer = styled.div<IProps>`
+	padding-bottom: 16px;
+	border-bottom: ${(props) => (props.activeQuestionTab ? "none" : "1px solid black")};
+	max-height: 300px;
+	transition: 0.3s;
+	overflow: hidden;
+`;
+
+export const QuestionType = styled(Text).attrs({ variant: "h4" })`
+	font-weight: 500;
+	font-size: 16px;
+	line-height: 24px;
+`;
+
+export const StyledText = styled(Text).attrs({
+	variant: "h4",
+})`
+	font-size: 14px;
+	font-weight: 500;
+	line-height: 21px;
+`;
+
+export const SingleQuestionsContainer = styled.div`
+	padding-left: 31px;
+	max-height: 300px;
+	overflow-y: scroll;
+	margin-top: 16px;
+	display: flex;
+	flex-direction: column;
+	gap: 16px;
+
+	/* Hide scrollbar for Chrome, Safari and Opera */
+	&::-webkit-scrollbar {
+		display: none;
+	}
+
+	/* Hide scrollbar for IE, Edge and Firefox */
+	& {
+		-ms-overflow-style: none; /* IE and Edge */
+		scrollbar-width: none; /* Firefox */
+	}
+`;
+
+export const SingleQuestions = styled(Grid).attrs({
+	container: true,
+	alignItems: "center",
+	justifyContent: "space-between",
+})`
+	padding: 0 8px 8px;
+	border-bottom: 1px solid #cdcdcd;
 `;
