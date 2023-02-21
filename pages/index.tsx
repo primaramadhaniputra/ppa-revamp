cdc97f21bdb25974910ed09d5131565b0ded3656
primaramadhaniputra <primaramadhanip@gmail.com>
2023-02-21 11:17:08 +0700

    fix fetching login page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,39 +1,29 @@
 import Loading from "atoms/Loading";
 import dynamic from "next/dynamic";
-import { useEffect, useState } from "react";
 import { currentLoginPages } from "services/pages";
-import { notify } from "utils/functions";
+import { useAsync } from "utils/customHooks";
+import { IPromiseResult } from "utils/interfaces";
 
 const LoginView = dynamic(() => import("views/Login"), { ssr: false, loading: () => <Loading /> });
 
 export default function LoginPage() {
-	const [id, setId] = useState();
-	const [isLoading, setIsLoading] = useState(false);
-
-	const getData = async () => {
-		try {
-			setIsLoading(true);
-			const data = await currentLoginPages({
+	const { loading, response } = useAsync(
+		() =>
+			currentLoginPages({
 				path: "pages",
 				params: {
 					name: "LOGIN",
 				},
-			});
-			const idLogin = data.data.data.pageId;
-			setId(idLogin);
-		} catch (error: any) {
-			notify(error.message, "error");
-		} finally {
-			setIsLoading(false);
-		}
-	};
-	useEffect(() => {
-		getData();
-	}, []);
+			}),
+		[],
+		[],
+		true,
+	);
 
-	if (isLoading) {
+	if (loading) {
 		return <Loading />;
 	}
+	const id = (response as IPromiseResult)?.data.data.pageId;
 
-	return <LoginView id={id || 1} />;
+	return <LoginView id={id || 0} />;
 }
