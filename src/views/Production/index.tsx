7f9e656d366684723c757569b46d40eac41a2b10
primaramadhaniputra <primaramadhanip@gmail.com>
2023-02-23 11:12:34 +0700

    fix filter periode

--- src/views/Production/index.tsx
+++ src/views/Production/index.tsx
@@ -1,7 +1,7 @@
 import { Grid } from "@hudoro/neron";
 import Loading from "atoms/Loading";
 import TitlePage from "atoms/TitlePage";
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { getAllSiteProduction } from "services/production";
 import { convert, notify } from "utils/functions";
 import { allSites, ITotalAllSites } from "utils/interfaces";
@@ -9,11 +9,12 @@ import Product from "./Product";
 import TopFilter from "./TopFIlter";
 
 export default function Production() {
-	const [activeTabs, setActiveTabs] = useState<number>();
+	const [activeTabs, setActiveTabs] = useState<string>('');
 	const [activeType, seActivetType] = useState(false);
 	const [sites, setSites] = useState<allSites[]>();
 	const [totalDataSites, setTotalDataSites] = useState<ITotalAllSites>();
 	const [isLoading, setIsLoading] = useState(true);
+
 	const [date, setDate] = useState([
 		{
 			startDate: new Date(),
@@ -22,47 +23,11 @@ export default function Production() {
 		},
 	]);
 
-	useEffect(() => {
-		if (activeTabs === 0) {
-			const date = new Date();
-			const firstDay = new Date(date.getFullYear(), date.getMonth(), 1);
-			setDate([
-				{
-					startDate: firstDay,
-					endDate: new Date(),
-					key: "selection",
-				},
-			]);
-		} else if (activeTabs === 1) {
-			const date = new Date();
-			date.setMonth(0);
-			const firstDay = new Date(date.getFullYear(), date.getMonth(), 1);
-			setDate([
-				{
-					startDate: firstDay,
-					endDate: new Date(),
-					key: "selection",
-				},
-			]);
-		} else if (activeTabs === 2) {
-			const date = new Date();
-			date.setDate(date.getDate() - 7);
-			setDate([
-				{
-					startDate: date,
-					endDate: new Date(),
-					key: "selection",
-				},
-			]);
-		}
-	}, [activeTabs]);
-
 	const getSites = async () => {
 		try {
 			setIsLoading(true);
 			const startTime = convert(Date.parse(date[0].startDate as unknown as string));
 			const endTime = convert(Date.parse(date[0].endDate as unknown as string));
-
 			const data = await getAllSiteProduction({
 				params: {
 					start: startTime,
@@ -82,7 +47,7 @@ export default function Production() {
 
 	useEffect(() => {
 		getSites();
-	}, [date, activeType]);
+	}, [date[0].endDate, activeType]);
 
 	const handleChangeActiveType = () => {
 		seActivetType(!activeType);
