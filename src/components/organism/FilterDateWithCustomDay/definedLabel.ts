d908c52da3b52562062df42887d8399520af31b0
primaramadhaniputra <primaramadhanip@gmail.com>
2023-03-08 10:19:56 +0700

    add custom filter date 1

--- src/components/organism/FilterDateWithCustomDay/definedLabel.ts
+++ src/components/organism/FilterDateWithCustomDay/definedLabel.ts
@@ -1,10 +1,6 @@
 import {
-	addDays,
-	endOfDay,
-	startOfDay,
 	startOfMonth,
 	endOfMonth,
-	addMonths,
 	startOfWeek,
 	endOfWeek,
 	isSameDay,
@@ -16,20 +12,10 @@ import {
 const defineds = {
 	startOfWeek: startOfWeek(new Date()),
 	endOfWeek: endOfWeek(new Date()),
-	startOfLastWeek: startOfWeek(addDays(new Date(), -7)),
-	endOfLastWeek: endOfWeek(addDays(new Date(), -7)),
-	startOfToday: startOfDay(new Date()),
-	endOfToday: endOfDay(new Date()),
-	startOfYesterday: startOfDay(addDays(new Date(), -1)),
-	endOfYesterday: endOfDay(addDays(new Date(), -1)),
 	startOfMonth: startOfMonth(new Date()),
 	endOfMonth: endOfMonth(new Date()),
-	startOfLastMonth: startOfMonth(addMonths(new Date(), -1)),
-	endOfLastMonth: endOfMonth(addMonths(new Date(), -1)),
 	startOfYears: startOfYear(addYears(new Date(), 0)),
 	endOfYears: endOfYear(addYears(new Date(), 0)),
-	startOfLastYears: startOfYear(addYears(new Date(), -1)),
-	endOfLastYears: endOfYear(addYears(new Date(), -1)),
 };
 
 const staticRangeHandler = {
@@ -49,60 +35,24 @@ export function createStaticRanges(ranges: any[]) {
 
 export const defaultStaticRanges = createStaticRanges([
 	{
-		label: "Today",
-		range: () => ({
-			startDate: defineds.startOfToday,
-			endDate: defineds.endOfToday,
-		}),
-	},
-	{
-		label: "Yesterday",
-		range: () => ({
-			startDate: defineds.startOfYesterday,
-			endDate: defineds.endOfYesterday,
-		}),
-	},
-
-	{
-		label: "This Week",
+		label: " Week To Date",
 		range: () => ({
 			startDate: defineds.startOfWeek,
 			endDate: defineds.endOfWeek,
 		}),
 	},
 	{
-		label: "Last Week",
-		range: () => ({
-			startDate: defineds.startOfLastWeek,
-			endDate: defineds.endOfLastWeek,
-		}),
-	},
-	{
-		label: "This Month",
+		label: "Month To Date",
 		range: () => ({
 			startDate: defineds.startOfMonth,
 			endDate: defineds.endOfMonth,
 		}),
 	},
 	{
-		label: "Last Month",
-		range: () => ({
-			startDate: defineds.startOfLastMonth,
-			endDate: defineds.endOfLastMonth,
-		}),
-	},
-	{
-		label: "This Years",
+		label: "Year To date",
 		range: () => ({
 			startDate: defineds.startOfYears,
 			endDate: defineds.endOfYears,
-		}),
-	},
-	{
-		label: "Last Years",
-		range: () => ({
-			startDate: defineds.startOfLastYears,
-			endDate: defineds.endOfLastYears,
 		}),
 	},
 ]);
--- src/components/organism/FilterDateWithCustomDay/index.tsx
+++ src/components/organism/FilterDateWithCustomDay/index.tsx
@@ -67,6 +67,7 @@ export default function FilteredDateWithCustomDay({
 						showDateDisplay={false}
 						staticRanges={defaultStaticRanges}
 						inputRanges={[]}
+						className="dateRange"
 					/>
 				</Container>
 			)}
