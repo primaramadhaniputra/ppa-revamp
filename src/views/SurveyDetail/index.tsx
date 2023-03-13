69640b49a1feb3ba283d1a95487ffc3c4abef27d
primaramadhaniputra <primaramadhanip@gmail.com>
2023-03-13 07:20:47 +0700

    add back menu

--- src/views/SurveyDetail/index.tsx
+++ src/views/SurveyDetail/index.tsx
@@ -1,6 +1,7 @@
-import { Grid } from "@hudoro/neron";
+import { Grid, Icon } from "@hudoro/neron";
 import TitlePage from "atoms/TitlePage";
 import Cookies from "js-cookie";
+import { useRouter } from "next/router";
 import { ISurveyReportCriteriaDetail, ISurveyReportCriticism } from "utils/interfaces";
 import CardDetail from "./CardDetail";
 import StatusCard from "./StatusCard";
@@ -15,20 +16,33 @@ const SurveyDetail = ({ dataReport, criticism }: IProps) => {
 	const site = Cookies.get("site");
 	const periode = Cookies.get("periode");
 	const parentCompany = Cookies.get("parentCompany");
+	const router = useRouter();
+
+	const handleBack = () => {
+		return router.back();
+	};
 
 	return (
 		<>
 			<Grid container gap={10} alignItems="center" justifyContent="space-between">
-				<TitlePage type="h3" styles={{ fontSize: "22px" }}>
-					Detail Laporan Survey /{" "}
-					<span style={{ fontSize: "16px", color: "rgba(170, 180, 198, 1)" }}>
-						{site} ({parentCompany})
-					</span>
-				</TitlePage>
+				<Grid container gap={10}>
+					<TitlePage type="h3" styles={{ fontSize: "22px" }}>
+						Detail Laporan Survey /{" "}
+						<span style={{ fontSize: "16px", color: "rgba(170, 180, 198, 1)" }}>
+							{site} ({parentCompany})
+						</span>
+					</TitlePage>
+				</Grid>
 				<PeriodeText>
 					periode : <span style={{ fontWeight: "600" }}>{periode}</span>
 				</PeriodeText>
 			</Grid>
+			<Icon
+				iconName="IcArrowLeft"
+				size={24}
+				style={{ marginTop: "15px", cursor: "pointer" }}
+				onClick={handleBack}
+			/>
 			<StatusCard />
 			<CardDetail dataReport={dataReport} criticism={criticism} />
 		</>
