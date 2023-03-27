c7fc980db191fbe9cd84f8591cecc5bb44f8ec02
primaramadhaniputra <primaramadhanip@gmail.com>
2023-03-27 13:09:14 +0700

    hide unused menu in page production / payload

--- src/views/Operation/Report/index.tsx
+++ src/views/Operation/Report/index.tsx
@@ -2,38 +2,40 @@ import { ISelectItem, Select } from "@hudoro/neron";
 import TitlePage from "atoms/TitlePage";
 import TabV1 from "molecules/TabV1";
 import dynamic from "next/dynamic";
-import React, { useState } from "react";
+import { useState } from "react";
 import { useWindowSize } from "utils/functions";
-import BaseControl from "./BaseControl";
-import Device from "./Device";
-import Mobile from "./Mobile";
-import Mocos from "./Mocos";
+// import BaseControl from "./BaseControl";
+// import Device from "./Device";
+// import Mobile from "./Mobile";
+// import Mocos from "./Mocos";
 import { ArrowDown, SelectContainer } from "./styles";
 
-const CostParameter = dynamic(() => import("./CostParameter"), { ssr: false });
-
-const tabsData = ["Device", "Mobile", "Moco", "Base Control", "Cost Parameter"];
+const Device = dynamic(() => import("./Device"), { ssr: false });
+// const CostParameter = dynamic(() => import("./CostParameter"), { ssr: false });
+// , "Mobile", "Moco", "Base Control", "Cost Parameter"
+const tabsData = ["Device"];
 
 const selectItems = [
 	{ id: 1, values: "Device", label: "Device" },
-	{ id: 2, values: "Mobile", label: "Mobile" },
-	{ id: 3, values: "Moco", label: "Moco" },
-	{ id: 4, values: "Base Control", label: "Base Control" },
-	{ id: 5, values: "Cost Parameter", label: "Cost Parameter" },
+	// { id: 2, values: "Mobile", label: "Mobile" },
+	// { id: 3, values: "Moco", label: "Moco" },
+	// { id: 4, values: "Base Control", label: "Base Control" },
+	// { id: 5, values: "Cost Parameter", label: "Cost Parameter" },
 ];
 
 function renderContent(queryName: string) {
-	if (queryName === "Device") {
-		return <Device />;
-	} else if (queryName === "Mobile") {
-		return <Mobile />;
-	} else if (queryName === "Moco") {
-		return <Mocos />;
-	} else if (queryName === "Base Control") {
-		return <BaseControl />;
-	} else {
-		return <CostParameter />;
-	}
+	console.log(queryName);
+	// if (queryName === "Device") {
+	return <Device />;
+	// } else if (queryName === "Mobile") {
+	// 	return <Mobile />;
+	// } else if (queryName === "Moco") {
+	// 	return <Mocos />;
+	// } else if (queryName === "Base Control") {
+	// 	return <BaseControl />;
+	// } else {
+	// 	return <CostParameter />;
+	// }
 }
 
 export default function Operation() {
