f23d1b13929c22f330d54e47d86c9ff2a5b019a9
prima ramadhani putra <primaramadhanip@gmail.com>
2023-06-06 08:43:22 +0700

    add table for data manajemen risiko

--- src/views/SurveyDetail/TableManajemenRisiko/index.tsx
+++ src/views/SurveyDetail/TableManajemenRisiko/index.tsx
@@ -1,9 +1,132 @@
 import React from "react";
-import { DescriptionContainer, Wrapper } from "./styles";
+import { DataTable, DescriptionContainer, TableContainer, Wrapper } from "./styles";
 import { IcEllipse } from "atoms/Icon";
 import { Grid, Text } from "@hudoro/neron";
+import {
+	createColumnHelper,
+	flexRender,
+	getCoreRowModel,
+	useReactTable,
+} from "@tanstack/react-table";
+
+type Person = {
+	firstName: string;
+	lastName: string;
+	age: number;
+	visits: number;
+	status: string;
+	progress: number;
+};
+
+const defaultData: Person[] = [
+	{
+		firstName: "tanner",
+		lastName: "linsley",
+		age: 24,
+		visits: 100,
+		status: "In Relationship",
+		progress: 50,
+	},
+	{
+		firstName: "tandy",
+		lastName: "miller",
+		age: 40,
+		visits: 40,
+		status: "Single",
+		progress: 80,
+	},
+	{
+		firstName: "joe",
+		lastName: "dirte",
+		age: 45,
+		visits: 20,
+		status: "Complicated",
+		progress: 10,
+	},
+];
+
+const columnHelper = createColumnHelper<Person>();
+
+const columns = [
+	columnHelper.accessor("firstName", {
+		cell: (info) => info.getValue(),
+		footer: (props) => props.column.id,
+		header: () => <span></span>,
+	}),
+	columnHelper.group({
+		id: "hello",
+		header: () => (
+			<>
+				<span style={{ lineHeight: "21px", color: "#969696" }}>Existing Busines</span>
+				<br />
+				<span style={{ lineHeight: "21px" }}> Mining Contractor</span>
+			</>
+		),
+		columns: [
+			columnHelper.accessor("firstName", {
+				cell: (info) => info.getValue(),
+				header: () => <IcEllipse width={15} style={{ color: "#29CC6A" }} />,
+			}),
+			columnHelper.accessor((row) => row.lastName, {
+				id: "lastName",
+				cell: (info) => info.getValue(),
+				header: () => <IcEllipse width={15} style={{ color: "#FC5555" }} />,
+			}),
+		],
+	}),
+	columnHelper.group({
+		id: "hello",
+		header: () => (
+			<>
+				<span style={{ lineHeight: "21px", color: "#969696" }}>Existing Busines</span>
+				<br />
+				<span style={{ lineHeight: "21px" }}> Mining Contractor</span>
+			</>
+		),
+		columns: [
+			columnHelper.accessor("firstName", {
+				cell: (info) => info.getValue(),
+				header: () => <IcEllipse width={15} style={{ color: "#29CC6A" }} />,
+			}),
+			columnHelper.accessor((row) => row.lastName, {
+				id: "lastName",
+				cell: (info) => info.getValue(),
+				header: () => <IcEllipse width={15} style={{ color: "#FC5555" }} />,
+			}),
+		],
+	}),
+	columnHelper.group({
+		id: "hello",
+		header: () => (
+			<>
+				<span style={{ lineHeight: "21px", color: "#969696" }}>Existing Busines</span>
+				<br />
+				<span style={{ lineHeight: "21px" }}> Mining Contractor</span>
+			</>
+		),
+		columns: [
+			columnHelper.accessor("firstName", {
+				cell: (info) => info.getValue(),
+				header: () => <IcEllipse width={15} style={{ color: "#29CC6A" }} />,
+			}),
+			columnHelper.accessor((row) => row.lastName, {
+				id: "lastName",
+				cell: (info) => info.getValue(),
+				header: () => <IcEllipse width={15} style={{ color: "#FC5555" }} />,
+			}),
+		],
+	}),
+];
 
 const TableManajemenRisiko = () => {
+	const [data, setData] = React.useState(() => [...defaultData]);
+
+	const table = useReactTable({
+		data,
+		columns,
+		getCoreRowModel: getCoreRowModel(),
+	} as any);
+
 	return (
 		<Wrapper>
 			<DescriptionContainer>
@@ -16,6 +139,32 @@ const TableManajemenRisiko = () => {
 					<Text variant="p">Jenis risiko yang berdampak (“negatif” atau “positif” ) besar. </Text>
 				</Grid>
 			</DescriptionContainer>
+			<TableContainer>
+				<DataTable>
+					<thead>
+						{table.getHeaderGroups().map((headerGroup) => (
+							<tr key={headerGroup.id}>
+								{headerGroup.headers.map((header) => (
+									<th key={header.id} colSpan={header.colSpan}>
+										{header.isPlaceholder
+											? null
+											: flexRender(header.column.columnDef.header, header.getContext())}
+									</th>
+								))}
+							</tr>
+						))}
+					</thead>
+					<tbody>
+						{table.getRowModel().rows.map((row) => (
+							<tr key={row.id}>
+								{row.getVisibleCells().map((cell) => (
+									<td key={cell.id}>{flexRender(cell.column.columnDef.cell, cell.getContext())}</td>
+								))}
+							</tr>
+						))}
+					</tbody>
+				</DataTable>
+			</TableContainer>
 		</Wrapper>
 	);
 };
--- src/views/SurveyDetail/TableManajemenRisiko/styles.ts
+++ src/views/SurveyDetail/TableManajemenRisiko/styles.ts
@@ -1,3 +1,4 @@
+import { fontFamilies } from "@hudoro/neron";
 import styled from "styled-components";
 
 export const Wrapper = styled.section`
@@ -6,5 +7,39 @@ margin-top:70px;
 export const DescriptionContainer = styled.article`
     display:flex;
     flex-direction:column;
-    gap:14px
+    gap:14px;
+`
+
+export const TableContainer = styled.div`
+margin-top:24px;
+background: #FFFFFF;
+box-shadow: 0px 4px 20px rgba(0, 0, 0, 0.25);
+border-radius: 15px;
+padding:16px 24px;
+`
+export const DataTable = styled.table`
+width:100%;
+font-family:${fontFamilies.poppins};
+& thead  {
+    border-bottom:1px solid #969696;
+    border-right:1px solid #969696;
+   
+}
+& thead tr th {
+    border-right:1px solid #969696;
+    padding:17px;
+}   
+& thead tr:nth-child(2) th {
+    /* border-top:2px solid #969696 */
+}   
+& tbody tr td{
+    border-right:1px solid #969696;
+    padding:17px;
+    text-align:center;
+    font-size:14px;
+    font-weight:400;
+}
+& tbody tr:nth-child(odd){
+    background-color:#f6f5f5
+}
 `
