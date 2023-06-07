b6d663635aa5d8fffcc24f9fdcee1ddd94e6975b
prima ramadhani putra <primaramadhanip@gmail.com>
2023-06-07 09:38:44 +0700

    add overflow css

--- src/views/SurveyDetail/TableManajemenRisiko/styles.ts
+++ src/views/SurveyDetail/TableManajemenRisiko/styles.ts
@@ -16,6 +16,7 @@ background: #FFFFFF;
 box-shadow: 0px 4px 20px rgba(0, 0, 0, 0.25);
 border-radius: 15px;
 padding:16px 24px;
+overflow:scroll;
 `
 export const DataTable = styled.table`
 width:100%;
