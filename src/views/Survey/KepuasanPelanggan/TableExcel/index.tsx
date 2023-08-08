3af4ec7f85cd8dfa11203e527b90d9d5aeebe226
prima ramadhani putra <primaramadhanip@gmail.com>
2023-08-08 07:31:21 +0700

    add hidden table

--- src/views/Survey/KepuasanPelanggan/TableExcel/index.tsx
+++ src/views/Survey/KepuasanPelanggan/TableExcel/index.tsx
@@ -91,7 +91,7 @@ const TableExcel = ({ }: IProps) => {
 			<Grid container justifyContent="flex-end" style={{ marginTop: "20px" }}>
 				<Button title="Download excel" onClick={onDownload} />
 			</Grid>
-			<Table ref={tableRef}>
+			<Table hidden ref={tableRef}>
 				<thead>
 					<tr>
 						<th>ppa</th>
