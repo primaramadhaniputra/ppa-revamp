b436721908d1555bd359afd46db8e9d06c542d78
prima ramadhani putra <primaramadhanip@gmail.com>
2023-08-10 09:02:20 +0700

    hidden table

--- src/views/SurveyDetail/index.tsx
+++ src/views/SurveyDetail/index.tsx
@@ -66,7 +66,7 @@ const SurveyDetail = () => {
 			<Grid container justifyContent="flex-end" style={{ marginTop: "20px" }}>
 				<Button title="Download Excel" onClick={onDownload} />
 			</Grid>
-			<table ref={tableRef}>
+			<table hidden ref={tableRef}>
 				<thead>
 					<tr>
 						<th>ppa</th>
