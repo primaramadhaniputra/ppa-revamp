f9d79f08286fae5ef2f1b5a927e94b878c4f4489
prima ramadhani putra <primaramadhanip@gmail.com>
2023-08-14 07:44:29 +0700

    fix ppa url

--- src/configs/appConfig.ts
+++ src/configs/appConfig.ts
@@ -5,7 +5,7 @@ const appEnvironment = {
 	api: {
 		baseurl: `${process.env.NEXT_PUBLIC_ROOT_API_URL}/v1`,
 		ppaurl: `${process.env.NEXT_PUBLIC_ROOT_API_URL_PPA}/v1`,
-		surveyurl: `${process.env.NEXT_PUBLIC_ROOT_API_URL_SURVEY}/v1`,
+		surveyurl: `${process.env.NEXT_PUBLIC_ROOT_API_URL_PPA_14}/v1`,
 		ssourl: `${process.env.NEXT_PUBLIC_ROOT_SSO_URL}`,
 		ppaurl11: `${process.env.NEXT_PUBLIC_ROOT_API_URL_PPA_11}`,
 		ppaurl19: `${process.env.NEXT_PUBLIC_ROOT_API_URL_PPA_19}/v1`,
