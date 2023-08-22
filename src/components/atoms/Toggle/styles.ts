bc9b4d2d98a6cbf3ce3bcf063737e9814e56303d
prima ramadhani putra <primaramadhanip@gmail.com>
2023-08-22 08:13:58 +0700

    recreate togle

--- src/components/atoms/Toggle/styles.ts
+++ src/components/atoms/Toggle/styles.ts
@@ -1,47 +1,41 @@
 import styled from "styled-components";
 
 export const Switch = styled.label`
-	position: relative;
-	display: inline-block;
 	width: 39px;
 	height: 20px;
+	position: relative;
+	cursor: pointer;
 
 	& input {
-		opacity: 0;
 		width: 0;
 		height: 0;
+		opacity: 0;
 	}
 
 	& .slider {
 		position: absolute;
-		cursor: pointer;
 		top: 0;
 		left: 0;
 		right: 0;
 		bottom: 0;
 		background-color: #d9d9d9;
-		-webkit-transition: 0.4s;
-		transition: 0.4s;
 		border-radius: 34px;
+		transition: 0.3s;
 	}
-
-	& .slider:before {
-		position: absolute;
+	& .slider::before {
 		content: "";
+		position: absolute;
 		height: 16px;
 		width: 16px;
-		left: 3px;
-		bottom: 2px;
 		background-color: white;
-		-webkit-transition: 0.4s;
-		transition: 0.4s;
 		border-radius: 50%;
+		bottom: 2px;
+		left: 2px;
+		transition: 0.3s;
 	}
-
 	& input:checked + .slider {
 		background-color: #141bbe;
 	}
-
 	& input:checked + .slider:before {
 		-webkit-transform: translateX(17px);
 		-ms-transform: translateX(17px);
