8965dc64968f6b83ac3e9d6e2b2a00e988e1190a
prima ramadhani putra <primaramadhanip@gmail.com>
2024-01-05 10:00:19 +0700

    remove unnecassary page

--- pages/dashboard/she/form/index.tsx
+++
@@ -1,8 +0,0 @@
-import Loading from "atoms/Loading";
-import dynamic from "next/dynamic";
-
-const SHEView = dynamic(() => import("views/SHE/Form"), { ssr: false, loading: () => <Loading /> });
-
-export default function SHEPage() {
-	return <SHEView />;
-}
--- src/views/SHE/Form/SapPlan/Table/FlyingForm/styles.ts
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components";
-
-export const InputContainer = styled.div`
-	display: flex;
-	flex-direction: column;
-	gap: 20px;
-`;
